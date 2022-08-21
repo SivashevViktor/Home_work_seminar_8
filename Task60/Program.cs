4aa4aebb6980d6446dbffa0f6aacbfafbff6e5bc
ViktorSivashev <viktor.sivashev@mail.ru>
2022-08-21 15:04:54 +0300

    task60

---
+++ Task60/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+int X = InputInt("Введите размер массива X :  ");
+int Y = InputInt("Введите размер массива Y : ");
+int Z = InputInt("Введите размер массива Z : ");
+int count = 100;
+
+if  (X * Y * Z > count)
+{
+    Console.Write("Массив слишком большой");
+    return;
+}
+
+int[,,] resultNums = Massive (X, Y, Z);
+
+for (int i = 0; i < resultNums.GetLength(0); i++)
+{
+    for (int j = 0; j < resultNums.GetLength(1); j++)
+    {
+        for (int k = 0; k < resultNums.GetLength(2); k++)
+        {
+            Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+int[,,] Massive(int size1, int size2, int size3)
+{
+    int[,,] array = new int[size1, size2, size3];
+    int[] values = new int[count];
+    int num
+     = 10;
+    for (int i = 0; i < values.Length; i++)
+        values[i] = num
+        ++;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomInd = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomInd];
+        values[randomInd] = temp;
+    }
+
+    int valueIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = values[valueIndex++];
+            }
+        }
+    }
+    return array;
+}
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
