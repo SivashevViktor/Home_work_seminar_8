90e85410bb7092e0183a38525e550ad25cd4fc72
ViktorSivashev <viktor.sivashev@mail.ru>
2022-08-21 13:36:32 +0300

    task56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -9,3 +9,49 @@
 // строки с наименьшей суммой элементов: 1 строка
 
 
+Console.WriteLine("Введите размер массива : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[,] num = new int[N, N];
+FillArrayRandomnum(num);
+PrintArray(num);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        sum = sum + num[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("Строка с наименьшей суммой элементов: " + (indexLine));
+
+void FillArrayRandomnum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
